7465ca2623d14ea91028ea5bc058a76fc2c82378
Wesley Alves <wesley.alves@sysmanager.com.br>
2024-06-03 16:26:41 -0300

    feat: adicionar nova feature header com endpoints e módulos
    Esta commit introduz uma nova feature de header, incluindo os seguintes endpoints e alterações:
    - Textos
    - Bandeira tarifária
    - Placeholder da barra de busca
    - Línguas disponíveis na aplicação
    
    Além disso, foram inseridos módulos que possibilitam o consumo dos menus do site.

---
+++ web/modules/custom/sys_twig_extensions/src/Plugin/rest/resource/HeaderResource.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\sys_twig_extensions\Plugin\rest\resource;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+
+/**
+ * Provides a resource for clients subscription to updates.
+ *
+ * @RestResource(
+ *   id = "custom_rest_resource_header",
+ *   label = @Translation("Header Endpoint"),
+ *   uri_paths = {
+ *     "canonical" = "/api/header",
+ *     "create" = "/api/header"
+ *   }
+ * )
+ */
+class HeaderResource extends ResourceBase {
+
+  /**
+   *
+   */
+  public function permissions() {
+    return [];
+  }
+
+  /**
+   *
+   */
+  public function get() {
+    return (new ResourceResponse([
+      'data' => [
+        'text' => [
+          'en' => theme_get_setting('text_en'),
+          'pt' => theme_get_setting('text')
+        ],
+        'tariff_band' => [
+          'label' => [
+            'en' => theme_get_setting('tariff_band_label'),
+            'pt' => theme_get_setting('tariff_band_label_en')
+          ],
+          'band' => theme_get_setting('band')
+        ],
+        'searchbar' => [
+          'en' => theme_get_setting('searchbar'),
+          'pt' => theme_get_setting('searchbar_en')
+        ]        
+      ] 
+    ]))->addCacheableDependency([
+      '#cache' => [
+        'max-age' => 0,
+      ],
+    ]);
+  }
+}
