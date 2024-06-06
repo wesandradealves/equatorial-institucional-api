2bd95df0f89bdf8348677e6e096832696b0911d0
Wesley Alves <wesley.alves@sysmanager.com.br>
2024-06-06 19:41:15 -0300

    feat: adicionar novos endpoints no rodapé para redes sociais, app stores e contatos
    Implementados novos endpoints na seção de rodapé para incluir links para plataformas de redes sociais, lojas de aplicativos e informações de contato.

---
+++ web/modules/custom/sys_twig_extensions/src/Plugin/rest/resource/FooterResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\sys_twig_extensions\Plugin\rest\resource;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Drupal\media\Entity\Media;
+use Drupal\file\Entity\File;
+
+/**
+ * Provides a resource for clients subscription to updates.
+ *
+ * @RestResource(
+ *   id = "custom_rest_resource_footer",
+ *   label = @Translation("Footer Endpoint"),
+ *   uri_paths = {
+ *     "canonical" = "/api/footer",
+ *     "create" = "/api/footer"
+ *   }
+ * )
+ */
+class FooterResource extends ResourceBase {
+
+  /**
+   *
+   */
+  public function permissions() {
+    return [];
+  }
+
+  /**
+   *
+   */
+  public function get() {
+
+    if(theme_get_setting('app_store_img')) {
+        $app_store_img = \Drupal::entityTypeManager()->getStorage('file')->load(theme_get_setting('app_store_img')[0])->getFileUri();
+        $app_store_img = \Drupal::service('file_url_generator')->generateAbsoluteString($app_store_img);
+    }
+
+    if(theme_get_setting('google_play_img')) {
+        $google_play_img = \Drupal::entityTypeManager()->getStorage('file')->load(theme_get_setting('google_play_img')[0])->getFileUri();
+        $google_play_img = \Drupal::service('file_url_generator')->generateAbsoluteString($google_play_img);
+    }
+    
+    if(theme_get_setting('talk_to_clara_img')) {
+        $talk_to_clara_img = \Drupal::entityTypeManager()->getStorage('file')->load(theme_get_setting('talk_to_clara_img')[0])->getFileUri();
+        $talk_to_clara_img = \Drupal::service('file_url_generator')->generateAbsoluteString($talk_to_clara_img);
+    }    
+
+    return (new ResourceResponse([
+      'data' => [
+        'social_networks' => [
+            'facebook' => theme_get_setting('facebook'),
+            'twitter' => theme_get_setting('twitter'),
+            'instagram' => theme_get_setting('instagram'),
+            'youtube' => theme_get_setting('youtube')
+        ],
+        'store' => [
+            'appstore' => [
+                'img' => isset($app_store_img) ? $app_store_img : null,
+                'url' => theme_get_setting('app_store_url'),
+            ],
+            'googleplay' => [
+                'img' => isset($google_play_img) ? $google_play_img : null,
+                'url' => theme_get_setting('google_play_url'),
+            ]                
+        ],
+        'contact' => [
+            'phone' => [
+                'pt' => theme_get_setting('phone'),
+                'en' => theme_get_setting('phone_en')
+            ],
+            'talktous' => [
+                'pt' => theme_get_setting('talk'),
+                'en' => theme_get_setting('talk_en'),
+                'url' => theme_get_setting('talk_url')
+            ],
+            'talktoclara' => [
+                'pt' => theme_get_setting('talk_to_clara'),
+                'en' => theme_get_setting('talk_to_clara_en'),
+                'url' => theme_get_setting('talk_to_clara_url'),
+                'img' => isset($talk_to_clara_img) ? $talk_to_clara_img : null
+            ]                          
+        ]        
+      ] 
+    ]))->addCacheableDependency([
+      '#cache' => [
+        'max-age' => 0,
+      ],
+    ]);
+  }
+}
