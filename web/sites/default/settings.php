4f46ae87482e2e2984ab329ead5ed0f3e871c595
Renato Alves Felix <rfelix@redhat.com>
2024-06-13 11:27:23 -0300

    dockerfile limpo

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -881,15 +881,4 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 
 $settings['config_sync_directory'] = 'config/sync';
 
-// $databases['default']['default'] = array(
-//   'database' => $_ENV['MYSQL_DATABASE'],
-//   'username' => $_ENV['MYSQL_USER'],
-//   'password' => $_ENV['MYSQL_PASSWORD'],
-//   'prefix' => '',
-//   'host' => $_ENV['MYSQL_HOST'],
-//   'port' => $_ENV['MYSQL_PORT'],
-//   'isolation_level' => 'READ COMMITTED',
-//   'driver' => 'mysql',
-//   'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-//   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-// );
+
