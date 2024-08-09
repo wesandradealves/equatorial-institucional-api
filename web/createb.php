a1b0ecfc6bccb6ae48a7a36d135e25d60b01153b
wanderley-ferreira <wanderleyfa@gmail.com>
2024-08-09 11:42:14 -0300

    update code base

---
+++ web/createb.php
@@ -0,0 +1,37 @@
+<?php
+// Configurações de conexão
+$host = 'drupal-postgres.institucional-drupal-ma-dev.svc.cluster.local';
+$dbname = 'drupal-database';
+$user = 'drupal-user';
+$password = 'drupal-pass';
+
+try {
+    // Conectar ao banco de dados
+    $dsn = "pgsql:host=$host;dbname=$dbname";
+    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+    // Script SQL de criação de tabela e índices
+    $sql = "
+
+
+
+
+CREATE TABLE IF NOT EXISTS db.locales_location (
+	lid bigserial NOT NULL,
+	sid int8 NOT NULL,
+	\"type\" varchar(50) DEFAULT ''::character varying NOT NULL,
+	\"name\" varchar(255) DEFAULT ''::character varying NOT NULL,
+	\"version\" varchar(20) DEFAULT 'none'::character varying NOT NULL,
+	CONSTRAINT idx_28383_primary PRIMARY KEY (lid)
+);
+CREATE INDEX idx_28383_string_type ON db.locales_location USING btree (sid, type);
+CREATE INDEX idx_28383_type_name ON db.locales_location USING btree (type, name);
+    ";
+
+    // Executar o script SQL
+    $pdo->exec($sql);
+
+    echo "Tabela e índices criados com sucesso!";
+} catch (PDOException $e) {
+    echo "Erro ao criar tabela e índices: " . $e->getMessage();
+}
