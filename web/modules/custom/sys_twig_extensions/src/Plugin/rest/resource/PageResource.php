3e578f426aff09f6e128e505b2fb5444a38b95f4
Wesley Alves <wesley.alves@sysmanager.com.br>
2024-08-15 02:54:13 -0300

    Adição do novo paragraph `Filter Carousel` permitindo o uso mais generalizado do bloco com filtros de taxonomia e carousel de algum tipo de conteúdo escolhido. - Alteração no resource de `Page` para receber query param `alias` ao invés de passar o alias pela url, corrigindo páginas com hierarquias na url

--- web/modules/custom/sys_twig_extensions/src/Plugin/rest/resource/PageResource.php
+++ web/modules/custom/sys_twig_extensions/src/Plugin/rest/resource/PageResource.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\sys_twig_extensions\Plugin\rest\resource;
+
 use Drupal\Core\Url;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
@@ -14,29 +15,42 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  *   id = "custom_rest_resource_page",
  *   label = @Translation("Pages Endpoint"),
  *   uri_paths = {
- *     "canonical" = "/api/page/{id}",
- *     "create" = "/api/page/{id}"
+ *     "canonical" = "/api/page",
+ *     "create" = "/api/page"
  *   }
  * )
  */
 class PageResource extends ResourceBase {
 
   /**
-   *
+   * Returns an array of permissions.
    */
   public function permissions() {
     return [];
   }
 
   /**
+   * Handles GET requests for the PageResource.
    *
+   * @param string $id
+   *   The node ID from the path.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request object.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   The response containing the node data.
    */
-  public function get($id) {
-    $path = \Drupal::service('path_alias.manager')->getPathByAlias("/$id");
+  public function get(Request $request) {
+    // Retrieve the 'alias' query parameter.
+    $alias = $request->query->get('alias');
+
+    // Convert the alias to an internal path.
+    $path = \Drupal::service('path_alias.manager')->getPathByAlias($alias);
     $params = Url::fromUri("internal:$path")->getRouteParameters();
     $entity_type = key($params);
     $node = \Drupal::entityTypeManager()->getStorage($entity_type)->load($params[$entity_type]);
 
+    // Return the node data with cacheable dependencies.
     return (new ResourceResponse($node))->addCacheableDependency([
       '#cache' => [
         'max-age' => 0,
